7c8ef924ea2535b94a8995b0691566449080ff37
davidandstephens <stephens.a.david@gmail.com>
2018-11-07 10:46:33 -0600

    finished statements exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,40 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumbers = 17;
+        System.out.println(myFavoriteNumbers);
+
+        String myString = "Hello from Java!";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+        // Post-increment
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        // Pre-increment
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4 * x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y -= x;
+
+//        byte charlie = 128;
+//        System.out.println(charlie);
+
     }
 
 }
