76928d78b31b66c3b301fde29470c8dd7c415f62
davidandstephens <stephens.a.david@gmail.com>
2018-11-07 15:10:37 -0600

    working on bonuses

---
+++ src/JustinExtrasIO.java
@@ -0,0 +1,47 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class JustinExtrasIO {
+
+//    https://www.javatpoint.com/java-program-to-count-the-number-of-words-in-a-string
+
+
+//    --------=========Problem 1=========--------
+//    private static int wordCount(String string)
+//    {
+//        int count=0;
+//
+//        char ch[]= new char[string.length()];
+//        for(int i=0;i<string.length();i++)
+//        {
+//            ch[i]= string.charAt(i);
+//            if( ((i>0)&&(ch[i]!=' ')&&(ch[i-1]==' ')) || ((ch[0]!=' ')&&(i==0)) )
+//                count++;
+//        }
+//        return count;
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Please enter your favorite quote:");
+//        String userQuote = sc.nextLine();
+//        System.out.println("How many words long is that quote?");
+//        int userGuess = sc.nextInt();
+//        if (userGuess == wordCount(userQuote)) {
+//            System.out.printf("\nQuote was %d words long.", wordCount(userQuote));
+//        } else {
+//            System.out.printf("\nYou guessed wrong... Quote was %d words long.", wordCount(userQuote));
+//        }
+//    }
+
+//    --------=========Problem 2=========--------
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Please list your three favorite foods!");
+//        String firstFood = scan.next();
+//        String secondFood = scan.next();
+//        String thirdFood = scan.next();
+//        System.out.printf("\n1) %s\n2) %s\n3) %s", firstFood,secondFood,thirdFood);
+//    }
+
+}
