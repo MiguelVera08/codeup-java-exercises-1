c654a4b0f436a3dc1fec58541c71b2129c029ce4
davidandstephens <stephens.a.david@gmail.com>
2018-12-07 15:53:28 -0600

    made a deal

--- src/DavidIsLazy.java
+++ src/DavidIsLazy.java
@@ -1,9 +1,10 @@
 public class DavidIsLazy {
     public static void main(String[] args) {
         int g = 27;
-        String message = "";
+        String message;
         for (int i = 1; i <= 26; i++) {
             for (int j = 1; j <= 26; j++) {
+                message = "";
                 if (i == j) {
                     break;
                 } else {
@@ -28,15 +29,15 @@ public class DavidIsLazy {
                     }
 
                     if (("" + g).length() == 1) {
-                        message += "('0000" + g + "', '";
+                        message += "\n('0000" + g + "', '";
                     } else if (("" + g).length() == 2) {
-                        message += "('000" + g + "', '";
+                        message += "\n('000" + g + "', '";
                     }  else if (("" + g).length() == 3) {
-                        message += "('00" + g + "', '";
+                        message += "\n('00" + g + "', '";
                     } else if (("" + g).length() == 4) {
-                        message += "('0" + g + "', '";
+                        message += "\n('0" + g + "', '";
                     } else {
-                        message += "('" + g + "', '";
+                        message += "\n('" + g + "', '";
                     }
 
                     if (("" + j).length() == 1) {
