f39915d2c79c8dfc8747047c5629aef0c943e080
davidandstephens <stephens.a.david@gmail.com>
2019-05-20 15:15:36 -0500

    made a small change

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,65 +5,55 @@ public class ControlFlowExercises {
 
 //        int i = 5;
 //        while (i < 15) {
-//            i++;
-//            System.out.println(i);
+//            System.out.println(i++);
 //        }
-
-//        double counter = 2;
+//
+//        double countNum = 2;
 //        do {
-//            counter = Math.pow(counter, 2);
-//            System.out.printf("%n%.0f", counter);
-//        } while (counter < 65536);
+//            countNum = Math.pow(countNum, 2);
+//            System.out.printf("%n%.0f", countNum);
+//        } while (countNum < 65536);
 
-        for (int i = 4; i <= 15; i++) {
-            System.out.println(i);
+        for (int j = 4; j <= 15; j++) {
+            System.out.println(j);
         }
 
         System.out.println("---------------------------------------");
 
-        for (double counter = 2; counter < 1000000; counter = Math.pow(counter, 2)) {
-            System.out.printf("%n%.0f", counter);
+        for (int counter = 2; counter < 1000000; counter = (int) Math.pow(counter, 2)) {
+            System.out.printf("%n%d", counter);
         }
         System.out.println(" ");
         System.out.println("---------------------------------------");
 
-        for (int i = 1; i < 101; i++) {
-            if (i % 5 == 0 && i % 3 == 0) {
+        for (int k = 1; k < 101; k++) {
+            if (k % 5 == 0 && k % 3 == 0) {
                 System.out.println("Fizzbuzz");
-            } else if (i % 3 == 0) {
+            } else if (k % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (i % 5 == 0) {
+            } else if (k % 5 == 0) {
                 System.out.println("Buzz");
             } else {
-                System.out.println(i);
+                System.out.println(k);
             }
         }
 
         System.out.println("---------------------------------------");
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("What number should we go up to?");
-        double userNum = sc.nextDouble();
-        System.out.println("number|squared|cubed ");
-        System.out.print("\n------|-------|------");
-        for (double i = 1; i<=userNum; i++) {
-            System.out.printf("%n%-5.0f |%-6.0f |%.0f", i, Math.pow(i,2), Math.pow(i,3));
-        }
-        System.out.println(" ");
-        System.out.println("Want to continue? [Y/N]");
-        String answer = sc.next();
-        while (answer.equalsIgnoreCase("y")) {
+        String answer;
+         do {
             System.out.println("What number should we go up to?");
             double newNum = sc.nextDouble();
             System.out.println("number|squared|cubed ");
             System.out.print("\n------|-------|------");
-            for (double i = 1; i<=newNum; i++) {
-                System.out.printf("%n%-5.0f |%-6.0f |%.0f", i, Math.pow(i,2), Math.pow(i,3));
+            for (double w = 1; w<=newNum; w++) {
+                System.out.printf("%n%-5.0f |%-6.0f |%.0f", w, Math.pow(w,2), Math.pow(w,3));
             }
             System.out.println(" ");
             System.out.println("Want to continue? [Y/N]");
             answer = sc.next();
-        }
+        } while (answer.equalsIgnoreCase("y"));
 
         System.out.println("---------------------------------------");
 
@@ -74,11 +64,11 @@ public class ControlFlowExercises {
         } else if (grade > 79) {
             System.out.println("User gets a B.");
         } else if (grade > 67) {
-            System.out.println("User gets a C...");
+            System.out.println("User gets a C.");
         } else if (grade > 59) {
-            System.out.println("User gets a D......");
+            System.out.println("User gets a D.");
         } else {
-            System.out.println("User is a failure in the game of life...");
+            System.out.println("User gets an F...");
         }
     }
 }
